2b1c02c3db7b58e8f1c4b3cda498779d58a2f05b
HuannAnd <huannvicente14@outlook.com>
2023-03-27 22:27:04 -0300

    Styling a Contact component and fixing images bugs

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,27 +1,47 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+
+import Image from "next/image";
+
+import styles from './styles.module.css';
 
 
 type ContactProps = {
-    children?: ReactNode
+    githubURL?: string,
+    linkedingURL?: string
 }
 
-export default function Contact({ children }: ContactProps) {
-    return (!children ? (
+export default function Contact({ githubURL, linkedingURL }: ContactProps) {
+    return (!githubURL && !linkedingURL ? (
         <p>There isn't contact here</p>
     ) : (
         <>
             <h2>Contact(s)</h2>
-            <div className="contacts">
-                <div className="contact">
-                    <img src="" alt="" />
-                </div>
-                <div className="contact">
-                    <img src="" alt="" />
-                </div>
-                <div className="contact">
-                    <img src="" alt="" />
-                </div>
-            </div>
+            <article className={styles.contactContainer}>
+                {githubURL && (
+                    <div className={styles.socialMedia}>
+                        <Image
+                            src="/assets/github-icon.svg"
+                            alt="Github icon"
+                            width={24}
+                            height={24}
+                            priority
+                        />
+                        <a className={styles.link} target='_blank' href={githubURL}>My GitHub Profile</a>
+                    </div>
+                )}
+                {linkedingURL && (
+                    <div className={styles.socialMedia}>
+                        <Image
+                            src="/assets/linkedin-icon.svg"
+                            alt="Linkedin icon"
+                            width={24}
+                            height={24}
+                            priority
+                        />
+                        <a className={styles.link} target='_blank' href={linkedingURL}>My Linkdin Profile</a>
+                    </div>
+                )}
+            </article>
         </>
     ))
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import utilStyles from '@/styles/utils.module.css'
 
-import { About, Layout, Date } from '@/components/index'
+import { About, Layout, Date, Contact } from '@/components/index'
 import { siteTitle } from '@/components/layout'
 
 
@@ -44,8 +44,11 @@ export default function Home({ allPostsData }: HomeProps) {
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <section className={`${styles.headingMd} ${utilStyles.padding1px}`}>
-        <About>I'm front-end developer, i like to many things, but the a thing, wich often i do, is listen songs! Its awesome, i'm, listen various genders, however im actually listen Rock songs.</About>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <About>I'm front-end developer, i like to many things, but the a thing, wich often i do, is listen songs! Its awesome, i'm listen various genders, however im actually listen Rock songs.</About>
+
+        <Contact githubURL='https://github.com/HuannAnd' linkedingURL='https://www.linkedin.com/in/huann-vicente-5092a9261/' />
+        
+        <h2 className={utilStyles.headingLg}>Blog Posts</h2>
         <ul style={{ left: '0' }}>
           {allPostsData.map(({ id, date, title }) =>
             <li style={{ listStyle: 'none' }} className={`${utilStyles.listItem} ${utilStyles.fontParagraph}`} key={id}>
@@ -57,8 +60,8 @@ export default function Home({ allPostsData }: HomeProps) {
             </li>
           )}
         </ul>
-      </section>
 
+      </section>
     </Layout>
   )
 }
